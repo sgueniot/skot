f0250428ff61cf100000c2a784d404794990a3c9
Sylvain Guéniot <sylvain.gueniot@gmail.com>
2022-02-08 09:07:21 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,7 @@
 #! /bin/sh
 touch build.gradle.kts
-pkg=${pkg:-my.pkg.app}
-name=${name:-My App}
-
-while [ $# -gt 0 ]; do
-
-   if [[ $1 == *"--"* ]]; then
-        param="${1/--/}"
-        declare $param="$2"
-        # echo $1 $2 // Optional to see the parameter:value result
-   fi
-
-  shift
-done
+pkg=$1
+name=$2
 
 echo $name
 
